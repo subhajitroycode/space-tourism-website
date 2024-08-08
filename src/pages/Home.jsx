263c948c0d11abd1d80fbac5a7293d71af5da56f
Subhajit Roy <roysubhajit8697@gmail.com>
2024-08-08 22:12:38 +0530

    navbar fixed & homepage created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="home-container">
+      <div className="hero-container">
+        <div className="home-content">
+          <h1 className="home-title">
+            So, you want to travel to
+            <span>Space</span>
+          </h1>
+          <p className="home-description">
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <div className="explore-container">
+          <button className="explore-btn">Explore</button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
