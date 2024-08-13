cea0f5689728a750b8dea922dd935a8fa605ea81
Subhajit Roy <roysubhajit8697@gmail.com>
2024-08-13 13:55:54 +0530

    crew pagination complete using swiperjs

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
 import { crew } from "../data.json";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Keyboard, Pagination, Navigation } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Crew = () => {
-  const [currentCrew, setCurrentCrew] = useState(2);
+  // const [currentCrew, setCurrentCrew] = useState(2);
 
   return (
     <section className="crew-container">
       <h1 className="heading-title">
         <span>02</span>meet your crew
       </h1>
-      <div className="crew-content">
-        <div className="crew-info">
+      <Swiper
+        slidesPerView={1}
+        keyboard={{ enabled: true }}
+        pagination={{ clickable: true }}
+        modules={[Pagination, Keyboard]}
+      >
+        {/* <div className="crew-info">
           <div className="crew-content-info">
             <h2>
               <span>{crew[currentCrew].role}</span>
@@ -30,8 +41,26 @@ const Crew = () => {
             src={crew[currentCrew].images.webp}
             alt={crew[currentCrew].name}
           />
-        </div>
-      </div>
+        </div> */}
+        {crew.map((member, id) => {
+          return (
+            <SwiperSlide key={id}>
+              <div className="crew-content">
+                <div className="crew-content-info">
+                  <h2>
+                    <span>{member.role}</span>
+                    {member.name}
+                  </h2>
+                  <p>{member.bio}</p>
+                </div>
+                <div className="crew-image">
+                  <img src={member.images.webp} alt={member.name} />
+                </div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </section>
   );
 };
